ee31709ed078aa85b16dd1b4040f16ee8ac74000
spedro-miguel <pedro.silva821@sptech.school>
2024-05-31 19:24:39 -0300

    atualizando cadastro

--- public/js/cadastro.js
+++ public/js/cadastro.js
@@ -1,3 +1,60 @@
+const form = document.getElementById("form");
+const campos = document.querySelectorAll(".required");
+const spans = document.querySelectorAll(".span-required");
+const emailRegex = /^\w+([-+.']\w+)*@\w+([-.]\w+)*\.\w+([-.]\w+)*$/;
+
+form.addEventListener("submit", (event) => {
+  event.preventDefault();
+  console.log("dsadad");
+  // nameValidate();
+  // emailValidate();
+  // mainPasswordValidate();
+  // comparePassword();
+});
+
+function setError(index) {
+  campos[index].style.border = "2px solid #e63636";
+  spans[index].style.display = "block";
+}
+
+function removeError(index) {
+  campos[index].style.border = "";
+  spans[index].style.display = "none";
+}
+
+function nameValidate() {
+  if (campos[0].value.length < 3) {
+    setError(0);
+  } else {
+    removeError(0);
+  }
+}
+
+function emailValidate() {
+  if (!emailRegex.test(campos[1].value)) {
+    setError(1);
+  } else {
+    removeError(1);
+  }
+}
+
+function mainPasswordValidate() {
+  if (campos[2].value.length < 7) {
+    setError(2);
+  } else {
+    removeError(2);
+    comparePassword();
+  }
+}
+
+function comparePassword() {
+  if (campos[2].value == campos[3].value && campos[3].value >= 7) {
+    removeError(3);
+  } else {
+    setError(3);
+  }
+}
+
 const inputs = document.querySelectorAll(".input");
 const button = document.querySelector(".cadastro__button");
 
@@ -13,16 +70,5 @@ const handleFocusOut = ({ target }) => {
   }
 };
 
-const handleChange = () => {
-  const [username, password, email] = inputs;
-
-  if (username.value && email.value && password.value.length >= 2) {
-    button.removeAttribute("disabled");
-  } else {
-    button.setAttribute("disabled", "");
-  }
-};
-
 inputs.forEach((input) => input.addEventListener("focus", handleFocus));
 inputs.forEach((input) => input.addEventListener("focusout", handleFocusOut));
-inputs.forEach((input) => input.addEventListener("input", handleChange));
